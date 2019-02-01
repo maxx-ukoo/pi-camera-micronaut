714707e87623bb5af3426a93a18441af4741ac68
Maksym Krasovskyi <maxx@maxx.net.ua>
2019-02-01 10:29:40 +0200

    Small updates

--- src/main/java/pi/camera/micronaut/controller/ImageController.java
+++ src/main/java/pi/camera/micronaut/controller/ImageController.java
@@ -1,12 +1,15 @@
 package pi.camera.micronaut.controller;
 
+import com.github.sarxos.webcam.ds.v4l4j.V4l4jDriver;
 import com.github.sarxos.webcam.Webcam;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.server.types.files.StreamedFile;
 
 import javax.imageio.ImageIO;
+
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -16,20 +19,36 @@ import java.io.InputStream;
 @Controller("/image")
 public class ImageController {
 
-    @Get
-    public StreamedFile download() {
-        // get default webcam and open it
-        Webcam webcam = Webcam.getDefault();
-        webcam.open();
-        BufferedImage image = webcam.getImage();
+	static {
+		Webcam.setDriver(new V4l4jDriver());
+	}
 
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(image, "PNG", os);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        InputStream inputStream = new ByteArrayInputStream(os.toByteArray());
-        return new StreamedFile(inputStream, "image.png");
-    }
+	@Get("/test")
+	public String index() {
+		return "Hello World";
+	}
+
+	@Get("/details")
+	public Dimension[] details() {
+		return Webcam.getDefault().getViewSizes();
+	}
+
+	@Get
+	public StreamedFile download(@QueryValue(defaultValue = "640") int width,
+			@QueryValue(defaultValue = "480") int height) throws IOException {
+		Webcam webcam = Webcam.getDefault();
+		if (webcam.isOpen()) {
+			webcam.close();
+		}
+		webcam.setViewSize(new Dimension(width, height));
+		webcam.open();
+		BufferedImage image = webcam.getImage();
+		if (webcam.isOpen()) {
+			webcam.close();
+		}
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		ImageIO.write(image, "PNG", os);
+		InputStream inputStream = new ByteArrayInputStream(os.toByteArray());
+		return new StreamedFile(inputStream, "image.png");
+	}
 }
